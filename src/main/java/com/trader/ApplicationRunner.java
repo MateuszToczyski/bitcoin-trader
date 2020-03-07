89704b90b6b610e7d9e258f26b825259c7157481
Mateusz Toczyski <toczyski.mateusz@gmail.com>
2020-03-07 19:27:56 +0100

    Modify project files

--- src/main/java/com/trader/ApplicationRunner.java
+++ src/main/java/com/trader/ApplicationRunner.java
@@ -4,16 +4,10 @@ import javafx.application.Application;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Tab;
-import javafx.scene.control.TabPane;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.VBox;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
-
-import java.util.concurrent.CompletableFuture;
 
 public class ApplicationRunner extends Application implements PriceObserver {
 
