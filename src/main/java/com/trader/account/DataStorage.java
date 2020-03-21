9ad588b3b2c0151835289672b7dc29564ad13cea
Mateusz Toczyski <toczyski.mateusz@gmail.com>
2020-03-21 18:26:18 +0100

    Remove unused methods

--- src/main/java/com/trader/account/DataStorage.java
+++ src/main/java/com/trader/account/DataStorage.java
@@ -1,14 +1,16 @@
 package com.trader.account;
 
-import com.google.gson.reflect.TypeToken;
-import com.trader.ApplicationRunner;
-import java.io.*;
-import java.lang.reflect.Type;
-import java.nio.file.*;
-import java.util.*;
 import com.google.gson.Gson;
-import com.google.gson.internal.LinkedTreeMap;
-import org.json.*;
+import com.google.gson.reflect.TypeToken;
+import org.json.JSONObject;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.lang.reflect.Type;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
 
 public class DataStorage {
 
@@ -46,45 +48,5 @@ public class DataStorage {
         try (PrintWriter writer = new PrintWriter(path)) {
             writer.println(jsonString);
         }
-    }
-
-    private List<Position> positionsFromLinkedTreeMapList(List<LinkedTreeMap<String, Object>> list) {
-
-        if(list.size() == 0) {
-            return null;
-        }
-
-        List<Position> positions = new ArrayList<>();
-
-        for(LinkedTreeMap<String, Object> map : list) {
-
-            Side side = Side.valueOf(map.get("side").toString());
-            double nominal = (double)map.get("nominal");
-            double openPrice = (double)map.get("openPrice");
-
-            positions.add(new Position(side, nominal, openPrice, ApplicationRunner.getMarginRequirement()));
-        }
-
-        return positions;
-    }
-
-    private List<Order> ordersFromLinkedTreeMapList(List<LinkedTreeMap<String, Object>> list) {
-
-        if(list.size() == 0) {
-            return null;
-        }
-
-        List<Order> orders = new ArrayList<>();
-
-        for(LinkedTreeMap<String, Object> map : list) {
-
-            Side side = Side.valueOf(map.get("side").toString());
-            double nominal = (double)map.get("nominal");
-            double price = (double)map.get("price");
-
-            orders.add(new Order(side, nominal, price));
-        }
-
-        return orders;
     }
 }
