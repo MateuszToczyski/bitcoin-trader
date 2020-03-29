12b87dd47370eeee20a50ae3311ede2f45459bad
Mateusz Toczyski <toczyski.mateusz@gmail.com>
2020-03-29 16:39:20 +0200

    Add a test case
    Test scenario: open and close a position

--- src/test/java/com/trader/AutomatedTestSuite.java
+++ src/test/java/com/trader/AutomatedTestSuite.java
@@ -2,8 +2,6 @@ package com.trader;
 
 import com.trader.account.*;
 import com.trader.exceptions.*;
-import com.trader.price.*;
-import com.trader.utils.*;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -20,13 +18,38 @@ public class AutomatedTestSuite {
         account.addPosition(Side.BUY, 10);
         account.update(5002, 5003);
 
+        Position position = account.openPositions().get(0);
+        Assertions.assertEquals(10, position.getProfit());
+
         Assertions.assertEquals(1, account.openPositions().size());
         Assertions.assertEquals(0, account.closedPositions().size());
         Assertions.assertEquals(0, account.orders().size());
 
-        Assertions.assertEquals(9499.9, account.getBalance(), 0.001);
-        Assertions.assertEquals(500.1, account.getMargin(), 0.001);
-        Assertions.assertEquals(10, account.getOpenProfit(), 0.001);
-        Assertions.assertEquals(20.016, account.getMarginLevel(), 0.001);
+        Assertions.assertEquals(9499.9, account.getBalance(), 0.0001);
+        Assertions.assertEquals(500.1, account.getMargin(), 0.0001);
+        Assertions.assertEquals(10, account.getOpenProfit(), 0.0001);
+        Assertions.assertEquals(20.016, account.getMarginLevel(), 0.0001);
+    }
+
+    @Test
+    public void testOpenAndClosePosition() {
+        Account account = new Account(0.01, 0.3, 10000, 0,
+                new LinkedList<>(), new LinkedList<>(), new LinkedList<>());
+
+        account.update(5000, 5001);
+        account.addPosition(Side.BUY, 10);
+        account.update(5002, 5003);
+
+        Position position = account.openPositions().get(0);
+        account.closePosition(position);
+
+        Assertions.assertEquals(0, account.openPositions().size());
+        Assertions.assertEquals(1, account.closedPositions().size());
+        Assertions.assertEquals(0, account.orders().size());
+
+        Assertions.assertEquals(10010, account.getBalance(), 0.0001);
+        Assertions.assertEquals(0, account.getMargin(), 0.0001);
+        Assertions.assertEquals(0, account.getOpenProfit(), 0.0001);
+        Assertions.assertEquals(0, account.getMarginLevel(), 0.0001);
     }
 }
